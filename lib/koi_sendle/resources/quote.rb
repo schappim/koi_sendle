2455f51a2e5bbd333a7f332846ce7c18d22003e2
Marcus Schappi <marcus@schappi.com>
2022-07-05 11:57:22 +1000

    add receiver country for internatinal quotes

--- lib/koi_sendle/resources/quote.rb
+++ lib/koi_sendle/resources/quote.rb
@@ -1,6 +1,6 @@
 module KoiSendle
   class QuoteResource < Resource
-    def get_quotes(product_code:, pickup_suburb:, pickup_postcode:, delivery_suburb:, delivery_postcode:,
+    def get_quotes(product_code:, pickup_suburb:, pickup_postcode:, delivery_suburb:, delivery_country:, delivery_postcode:,
       weight_value:, weight_units:)
 
       params = {
@@ -9,6 +9,7 @@ module KoiSendle
         pickup_postcode: pickup_postcode,
         delivery_suburb: delivery_suburb,
         delivery_postcode: delivery_postcode,
+        delivery_country: delivery_country,
         weight_value: weight_value,
         weight_units: weight_units
       }
