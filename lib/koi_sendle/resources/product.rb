b6f1dbff565b746798b83dab92c3daa211a3f9c2
John Croucher <john@jcroucher.com>
2023-08-31 14:13:47 +0930

    add volume support

--- lib/koi_sendle/resources/product.rb
+++ lib/koi_sendle/resources/product.rb
@@ -1,7 +1,7 @@
 module KoiSendle
   class ProductResource < Resource
     def get_products(sender_suburb:, sender_postcode:, sender_country:, receiver_suburb:, receiver_postcode:, receiver_country:,
-      weight_value:, weight_units:)
+      weight_value:, weight_units:, volume_value: nil, volume_units: nil)
 
       params = {
         sender_suburb: sender_suburb,
@@ -13,6 +13,9 @@ module KoiSendle
         weight_value: weight_value,
         weight_units: weight_units
       }
+
+      params[:volume_value] = volume_value if volume_value.present?
+      params[:volume_units] = volume_units if volume_units.present?
 
       resp = get_request("products", params: params).body
       puts resp
